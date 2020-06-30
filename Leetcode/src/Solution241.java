52d6f9dae7338f057bb580a1c40f7ebd2d1c2492
DragonistYJ <1121498469@qq.com>
2020-06-30 15:02:28 +0800

    2020.6.30 分治

---
+++ Leetcode/src/Solution241.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+/**
+ * 为运算表达式设计优先级
+ * 给定一个含有数字和运算符的字符串，为表达式添加括号，改变其运算优先级以求出不同的结果。你需要给出所有可能的组合的结果。有效的运算符号包含 +, - 以及 * 。
+ */
+public class Solution241 {
+    private List<Integer> numList;
+    private List<Character> operatorList;
+    private ArrayList[][] memo;
+
+    private int calc(int num1, int num2, char operator) {
+        switch (operator) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            default:
+                return num1 * num2;
+        }
+    }
+
+    private List<Integer> rec(int start, int end) {
+        if (memo[start][end] != null) {
+            return memo[start][end];
+        }
+
+        if (start == end) {
+            memo[start][end] = new ArrayList<>(Collections.singleton(numList.get(start)));
+            return memo[start][end];
+        }
+
+        ArrayList ansList = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            List<Integer> leftList = rec(start, i);
+            List<Integer> rightList = rec(i + 1, end);
+            for (Integer leftNum : leftList) {
+                for (Integer rightNum : rightList) {
+                    ansList.add(calc(leftNum, rightNum, operatorList.get(i)));
+                }
+            }
+        }
+
+        memo[start][end] = ansList;
+        return ansList;
+    }
+
+    // 拆分成数字和运算符两个数组
+    private void splitString(String str) {
+        numList = new ArrayList<>();
+        operatorList = new ArrayList<>();
+        int num = 0;
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                numList.add(num);
+                operatorList.add(c);
+                num = 0;
+            } else {
+                num *= 10;
+                num += c - '0';
+            }
+        }
+
+        numList.add(num);
+    }
+
+    public List<Integer> diffWaysToCompute(String input) {
+        splitString(input);
+        int len = numList.size();
+        memo = new ArrayList[len][len];
+        return rec(0, len - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution241().diffWaysToCompute("2*3-4*5"));
+    }
+}
---
+++ Leetcode/src/Solution793.java
@@ -0,0 +1,23 @@
+/**
+ * 阶乘函数后K个零
+ * f(x) 是 x! 末尾是0的数量。（回想一下 x! = 1 * 2 * 3 * ... * x，且0! = 1）
+ * 例如， f(3) = 0 ，因为3! = 6的末尾没有0；而 f(11) = 2 ，因为11!= 39916800末端有2个0。给定 K，找出多少个非负整数x ，有 f(x) = K 的性质。
+ */
+public class Solution793 {
+    public int preimageSizeFZF(long K) {
+        long lo = K, hi = 10*K + 1;
+        while (lo < hi) {
+            long mi = lo + (hi - lo) / 2;
+            long zmi = zeta(mi);
+            if (zmi == K) return 5;
+            else if (zmi < K) lo = mi + 1;
+            else hi = mi;
+        }
+        return 0;
+    }
+
+    public long zeta(long x) {
+        if (x == 0) return 0;
+        return x/5 + zeta(x/5);
+    }
+}
